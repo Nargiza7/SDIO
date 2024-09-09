6c7c65161b3bc4b9790b57b6d5615290eb448ee8
nargizajyrgal <83071723+Nargiza7@users.noreply.github.com>
2024-09-09 11:36:04 +0300

    commit

---
+++ Core/Src/main.c
@@ -0,0 +1,387 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "fatfs.h"
+#include "usart.h"
+#include "sdmmc.h"
+#include "usb_otg.h"
+#include "gpio.h"
+#include "stdio.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "string.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+FIL myFile;
+FRESULT res;
+UINT bytesWritten;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+//void WriteToSDCard(void);
+void FormatAndWriteToSDCard(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+#include "stm32l4xx_hal.h"
+
+void FormatAndCheckSDCard(void) {
+    FATFS FatFs;
+    FIL fil;
+    DIR dir;
+    FILINFO fno;
+    FRESULT res;
+    BYTE work[_MAX_SS];
+    char buffer[100];
+
+    res = f_mount(&FatFs, "", 0);
+    if (res != FR_OK) {
+        snprintf(buffer, sizeof(buffer), "SD kart montaji basarisiz oldu: %d\r\n", res);
+        HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+        return;
+    }
+
+    res = f_mkfs("", FM_FAT32, 0, work, sizeof(work));
+    if (res != FR_OK) {
+        snprintf(buffer, sizeof(buffer), "SD kart formatlama basarisiz oldu: %d\r\n", res);
+        HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+        return;
+    } else {
+        snprintf(buffer, sizeof(buffer), "SD kart basariyla formatlandi.\r\n");
+        HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+    }
+
+
+    res = f_mount(&FatFs, "", 1);
+    if (res != FR_OK) {
+        snprintf(buffer, sizeof(buffer), "SD kart montaji basarisiz oldu: %d\r\n", res);
+        HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+        return;
+    }
+
+    res = f_open(&fil, "program.txt", FA_CREATE_ALWAYS | FA_WRITE);
+    if (res == FR_OK) {
+        char writeText[] = "Hello STM32";
+        UINT bytesWritten;
+        res = f_write(&fil, writeText, strlen(writeText), &bytesWritten);
+        f_close(&fil);
+
+        if (res == FR_OK && bytesWritten == strlen(writeText)) {
+            snprintf(buffer, sizeof(buffer), "Dosya basariyla olusturuldu ve yazildi.\r\n");
+            HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+        } else {
+            snprintf(buffer, sizeof(buffer), "Dosyaya yazma basarisiz oldu: %d\r\n", res);
+            HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+        }
+    } else {
+        snprintf(buffer, sizeof(buffer), "Dosya olusturma basarisiz oldu: %d\r\n", res);
+        HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+        return;
+    }
+
+//    res = f_opendir(&dir, "/");
+//    if (res == FR_OK) {
+//        while (1) {
+//            res = f_readdir(&dir, &fno);
+//            if (res != FR_OK || fno.fname[0] == '\0') break;
+//
+//            snprintf(buffer, sizeof(buffer), "Dosya bulundu: %s\r\n", fno.fname);
+//            HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+//        }
+//        f_closedir(&dir);
+//
+//        if (fno.fname[0] == '\0') {
+//            snprintf(buffer, sizeof(buffer), "Format basarili, kok dizin bos.\r\n");
+//            HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+//        } else {
+//            snprintf(buffer, sizeof(buffer), "Kok dizin bos degil.\r\n");
+//            HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+//        }
+//    } else {
+//        snprintf(buffer, sizeof(buffer), "Kok dizin acilamadi: %d\r\n", res);
+//        HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+//    }
+
+    f_mount(NULL, "", 0);
+}
+
+
+//void WriteToSDCard(void) {
+//    HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+//    HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+//
+//    const char *message = "Merhaba STM32!\r\n";
+//
+//    if (FATFS_GetAttachedDriversNbr() < _VOLUMES) MX_FATFS_Init();
+//
+//    if (f_mount(&SDFatFS, SDPath, 1) == FR_OK) {
+//
+//        res = f_open(&myFile, "test.txt", FA_OPEN_APPEND | FA_WRITE);
+//        if (res == FR_OK) {
+//            res = f_write(&myFile, message, strlen(message), &bytesWritten);
+//            if (res == FR_OK) {
+//
+//                f_close(&myFile);
+//                HAL_UART_Transmit(&hlpuart1, (uint8_t *)"Dosya basariyla yazdirildi!\r\n", 26, HAL_MAX_DELAY);
+//                HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+//            } else {
+//
+//                HAL_UART_Transmit(&hlpuart1, (uint8_t *)"Dosya yazma hatasi\r\n", 21, HAL_MAX_DELAY);
+//                HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+//            }
+//        } else {
+//
+//            HAL_UART_Transmit(&hlpuart1, (uint8_t *)"Dosya acilamadi.\r\n", 18, HAL_MAX_DELAY);
+//            HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+//        }
+//
+//        f_mount(0, "", 0);
+//    } else {
+//
+//    	if (FATFS_GetAttachedDriversNbr() > 0)
+//    		FATFS_UnLinkDriver(SDPath);
+//
+//        HAL_UART_Transmit(&hlpuart1, (uint8_t *)"SD Kart mount edilemedi.\r\n", 27, HAL_MAX_DELAY);
+//        HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+//    }
+//}
+
+//void FormatAndWriteToSDCard(void) {
+//    FATFS SDFatFS;   // File system object
+//    FIL myFile;      // File object
+//    FRESULT res;     // Function result
+//    BYTE work[_MAX_SS];  // Working buffer
+//    UINT bytesWritten;
+//    const char *message = "Merhaba STM32!\r\n";
+//
+//    HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+//    HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+//
+//    if (FATFS_GetAttachedDriversNbr() < _VOLUMES) MX_FATFS_Init();
+//
+//    res = f_mount(&SDFatFS, SDPath, 0);
+//    if (res != FR_OK) {
+//        // SD kart montajı başarısız oldu
+//        HAL_UART_Transmit(&hlpuart1, (uint8_t *)"SD Kart montajı başarısız.\r\n", 29, HAL_MAX_DELAY);
+//        HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+//        return;
+//    }
+//
+//    res = f_mkfs("", FM_FAT32, 0, work, sizeof(work));
+//    if (res != FR_OK) {
+//        // Formatlama başarısız oldu
+//        HAL_UART_Transmit(&hlpuart1, (uint8_t *)"SD Kart formatlama başarısız.\r\n", 32, HAL_MAX_DELAY);
+//        HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+//        return;
+//    } else {
+//        HAL_UART_Transmit(&hlpuart1, (uint8_t *)"SD Kart başarıyla formatlandı.\r\n", 33, HAL_MAX_DELAY);
+//    }
+//
+//    // 4. SD kartı tekrar monte edin
+//    res = f_mount(&SDFatFS, SDPath, 1);
+//    if (res != FR_OK) {
+//        // Format sonrası montaj başarısız oldu
+//        HAL_UART_Transmit(&hlpuart1, (uint8_t *)"SD Kart montajı başarısız (format sonrası).\r\n", 47, HAL_MAX_DELAY);
+//        HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+//        return;
+//    }
+//
+//    // 5. Test dosyasını aç ve "Merhaba STM32!" yaz
+//    res = f_open(&myFile, "test.txt", FA_CREATE_ALWAYS | FA_WRITE);
+//    if (res == FR_OK) {
+//        res = f_write(&myFile, message, strlen(message), &bytesWritten);
+//        if (res == FR_OK && bytesWritten == strlen(message)) {
+//            // Dosya başarıyla yazıldı
+//            f_close(&myFile);
+//            HAL_UART_Transmit(&hlpuart1, (uint8_t *)"Dosya başarıyla yazdırıldı!\r\n", 30, HAL_MAX_DELAY);
+//            HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);  // Başarı LED'i
+//        } else {
+//            // Dosyaya yazma hatası
+//            HAL_UART_Transmit(&hlpuart1, (uint8_t *)"Dosya yazma hatası.\r\n", 21, HAL_MAX_DELAY);
+//            HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);  // Hata LED'i
+//        }
+//    } else {
+//        // Dosya açma hatası
+//        HAL_UART_Transmit(&hlpuart1, (uint8_t *)"Dosya açılamadı.\r\n", 18, HAL_MAX_DELAY);
+//        HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);  // Hata LED'i
+//    }
+//
+//    // 6. SD kartın bağlantısını kapatın
+//    f_mount(NULL, "", 0);
+//    if (FATFS_GetAttachedDriversNbr() > 0)
+//        FATFS_UnLinkDriver(SDPath);
+//}
+
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_LPUART1_UART_Init();
+  MX_USB_OTG_FS_PCD_Init();
+  MX_SDMMC1_SD_Init();
+  MX_FATFS_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  FormatAndCheckSDCard();
+  while (1)
+  {
+	 HAL_Delay(500);
+	// WriteToSDCard();
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ Core/Src/sdmmc.c
@@ -0,0 +1,221 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    sdmmc.c
+  * @brief   This file provides code for the configuration
+  *          of the SDMMC instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "sdmmc.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+SD_HandleTypeDef hsd1;
+DMA_HandleTypeDef hdma_sdmmc1_rx;
+DMA_HandleTypeDef hdma_sdmmc1_tx;
+
+/* SDMMC1 init function */
+
+void MX_SDMMC1_SD_Init(void)
+{
+
+  /* USER CODE BEGIN SDMMC1_Init 0 */
+
+  /* USER CODE END SDMMC1_Init 0 */
+
+  /* USER CODE BEGIN SDMMC1_Init 1 */
+
+  /* USER CODE END SDMMC1_Init 1 */
+  hsd1.Instance = SDMMC1;
+  hsd1.Init.ClockEdge = SDMMC_CLOCK_EDGE_RISING;
+  hsd1.Init.ClockBypass = SDMMC_CLOCK_BYPASS_DISABLE;
+  hsd1.Init.ClockPowerSave = SDMMC_CLOCK_POWER_SAVE_DISABLE;
+  hsd1.Init.BusWide = SDMMC_BUS_WIDE_1B;
+  hsd1.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_DISABLE;
+  hsd1.Init.ClockDiv = 0;
+  /* USER CODE BEGIN SDMMC1_Init 2 */
+
+  /* USER CODE END SDMMC1_Init 2 */
+
+}
+
+void HAL_SD_MspInit(SD_HandleTypeDef* sdHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  if(sdHandle->Instance==SDMMC1)
+  {
+  /* USER CODE BEGIN SDMMC1_MspInit 0 */
+
+  /* USER CODE END SDMMC1_MspInit 0 */
+
+  /** Initializes the peripherals clock
+  */
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SDMMC1;
+    PeriphClkInit.Sdmmc1ClockSelection = RCC_SDMMC1CLKSOURCE_HSI48;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* SDMMC1 clock enable */
+    __HAL_RCC_SDMMC1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    /**SDMMC1 GPIO Configuration
+    PC8     ------> SDMMC1_D0
+    PC12     ------> SDMMC1_CK
+    PD2     ------> SDMMC1_CMD
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_12;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF12_SDMMC1;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF12_SDMMC1;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+    /* SDMMC1 DMA Init */
+    /* SDMMC1_RX Init */
+    hdma_sdmmc1_rx.Instance = DMA2_Channel4;
+    hdma_sdmmc1_rx.Init.Request = DMA_REQUEST_7;
+    hdma_sdmmc1_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_sdmmc1_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_sdmmc1_rx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_sdmmc1_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+    hdma_sdmmc1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
+    hdma_sdmmc1_rx.Init.Mode = DMA_NORMAL;
+    hdma_sdmmc1_rx.Init.Priority = DMA_PRIORITY_LOW;
+    if (HAL_DMA_Init(&hdma_sdmmc1_rx) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(sdHandle,hdmarx,hdma_sdmmc1_rx);
+
+    /* SDMMC1_TX Init */
+    hdma_sdmmc1_tx.Instance = DMA2_Channel5;
+    hdma_sdmmc1_tx.Init.Request = DMA_REQUEST_7;
+    hdma_sdmmc1_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+    hdma_sdmmc1_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_sdmmc1_tx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_sdmmc1_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+    hdma_sdmmc1_tx.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
+    hdma_sdmmc1_tx.Init.Mode = DMA_NORMAL;
+    hdma_sdmmc1_tx.Init.Priority = DMA_PRIORITY_LOW;
+    if (HAL_DMA_Init(&hdma_sdmmc1_tx) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(sdHandle,hdmatx,hdma_sdmmc1_tx);
+
+    /* SDMMC1 interrupt Init */
+    HAL_NVIC_SetPriority(SDMMC1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(SDMMC1_IRQn);
+  /* USER CODE BEGIN SDMMC1_MspInit 1 */
+
+  /* USER CODE END SDMMC1_MspInit 1 */
+  }
+}
+
+void HAL_SD_MspDeInit(SD_HandleTypeDef* sdHandle)
+{
+
+  if(sdHandle->Instance==SDMMC1)
+  {
+  /* USER CODE BEGIN SDMMC1_MspDeInit 0 */
+
+  /* USER CODE END SDMMC1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SDMMC1_CLK_DISABLE();
+
+    /**SDMMC1 GPIO Configuration
+    PC8     ------> SDMMC1_D0
+    PC12     ------> SDMMC1_CK
+    PD2     ------> SDMMC1_CMD
+    */
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_8|GPIO_PIN_12);
+
+    HAL_GPIO_DeInit(GPIOD, GPIO_PIN_2);
+
+    /* SDMMC1 DMA DeInit */
+    HAL_DMA_DeInit(sdHandle->hdmarx);
+    HAL_DMA_DeInit(sdHandle->hdmatx);
+
+    /* SDMMC1 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(SDMMC1_IRQn);
+  /* USER CODE BEGIN SDMMC1_MspDeInit 1 */
+
+  /* USER CODE END SDMMC1_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+uint8_t BSP_SD_WriteBlocks_DMA(uint32_t *pData, uint32_t WriteAddr, uint32_t NumOfBlocks)
+{
+  uint8_t sd_state = MSD_OK;
+
+
+  hsd1.hdmarx->ChannelIndex = (((uint32_t)hsd1.hdmarx->Instance - (uint32_t)DMA2_Channel1) / ((uint32_t)DMA2_Channel2 - (uint32_t)DMA2_Channel1)) << 2U;
+  DMA2_CSELR->CSELR &= ~(DMA_CSELR_C1S << (hsd1.hdmarx->ChannelIndex & 0x1cU));
+
+
+  hsd1.hdmatx->ChannelIndex = (((uint32_t)hsd1.hdmatx->Instance - (uint32_t)DMA2_Channel1) / ((uint32_t)DMA2_Channel2 - (uint32_t)DMA2_Channel1)) << 2U;
+  DMA2_CSELR->CSELR &= ~(DMA_CSELR_C1S << (hsd1.hdmatx->ChannelIndex & 0x1cU));
+  DMA2_CSELR->CSELR |= (uint32_t)(hsd1.hdmatx->Init.Request << (hsd1.hdmatx->ChannelIndex & 0x1cU));
+
+
+  /* Write block(s) in DMA transfer mode */
+  if (HAL_SD_WriteBlocks_DMA(&hsd1, (uint8_t *)pData, WriteAddr, NumOfBlocks) != HAL_OK)
+  {
+    sd_state = MSD_ERROR;
+  }
+
+  return sd_state;
+}
+
+uint8_t BSP_SD_ReadBlocks_DMA(uint32_t *pData, uint32_t ReadAddr, uint32_t NumOfBlocks)
+{
+  uint8_t sd_state = MSD_OK;
+
+  /* Read block(s) in DMA transfer mode */
+
+  hsd1.hdmatx->ChannelIndex = (((uint32_t)hsd1.hdmatx->Instance - (uint32_t)DMA2_Channel1) / ((uint32_t)DMA2_Channel2 - (uint32_t)DMA2_Channel1)) << 2U;
+  DMA2_CSELR->CSELR &= ~(DMA_CSELR_C1S << (hsd1.hdmatx->ChannelIndex & 0x1cU));
+
+
+  hsd1.hdmarx->ChannelIndex = (((uint32_t)hsd1.hdmarx->Instance - (uint32_t)DMA2_Channel1) / ((uint32_t)DMA2_Channel2 - (uint32_t)DMA2_Channel1)) << 2U;
+  DMA2_CSELR->CSELR &= ~(DMA_CSELR_C1S << (hsd1.hdmarx->ChannelIndex & 0x1cU));
+  DMA2_CSELR->CSELR |= (uint32_t)(hsd1.hdmarx->Init.Request << (hsd1.hdmarx->ChannelIndex & 0x1cU));
+
+  if (HAL_SD_ReadBlocks_DMA(&hsd1, (uint8_t *)pData, ReadAddr, NumOfBlocks) != HAL_OK)
+  {
+    sd_state = MSD_ERROR;
+  }
+
+  return sd_state;
+}
+/* USER CODE END 1 */
